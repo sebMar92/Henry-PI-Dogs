0197539fb06694ecb0ef3ce5e60a21b1471eefd0
unknown <sebmar92@gmail.com>
2022-02-18 01:33:55 -0300

    stable

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 //import { useState, useEffect } from "react";
 //import { useDispatch, useSelector } from "react-redux";
-import Filter from "../Filter/Filter.jsx"
-import CardDisplay from "../CardDisplay/CardDisplay.jsx"
-import "./Home.css"
+import Filter from "../Filter/Filter.jsx";
+import CardDisplay from "../CardDisplay/CardDisplay.jsx";
+import "./Home.css";
 
-export default function Home(){
-    return (
-        <div className="home">
-            <Filter />
-            <CardDisplay />
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="home">
+      <Filter />
+      <CardDisplay />
+    </div>
+  );
 }
